d1ede8d2f855be171995cf7228d8f5196d5aee9b
madv123 <97916721+madv123@users.noreply.github.com>
2023-10-17 23:30:12 +0200

    Update DestinationSelectionActivity.java

--- app/src/main/java/com/example/travelappoctober2023/DestinationSelectionActivity.java
+++ app/src/main/java/com/example/travelappoctober2023/DestinationSelectionActivity.java
@@ -1,14 +1,19 @@
+
 package com.example.travelappoctober2023;
 
+// Importation biblio 
 import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
+
 
 public class DestinationSelectionActivity extends AppCompatActivity {
 
+  
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // appell la méthode onCreate 
         super.onCreate(savedInstanceState);
+     
         setContentView(R.layout.activity_destination_selection);
     }
 }
